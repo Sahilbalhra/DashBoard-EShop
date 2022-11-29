b1be2225a47a24d490fc5180f57b26b6534e6920
Sahilbalhra <sahilbalhra0@gmail.com>
2022-11-29 16:19:26 +0530

    Tables added

--- src/Pages/list/List.jsx
+++ src/Pages/list/List.jsx
@@ -1,11 +1,19 @@
-import React from 'react'
-import "./list.scss"
+import React from "react";
+import "./list.scss";
+import Sidebar from "../../components/sidebar/Sidebar";
+import Navbar from "../../components/Navbar/Navbar";
+import DataTable from "../../components/datatable/DataTable";
+
 const List = () => {
   return (
-    <div>
-      List
+    <div className='list'>
+      <Sidebar />
+      <div className='listContainer'>
+        <Navbar />
+        <DataTable />
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default List
+export default List;
