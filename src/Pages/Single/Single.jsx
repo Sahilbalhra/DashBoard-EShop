84b1b83423836cc164ad7afdcce61b7a18542d93
Sahilbalhra <sahilbalhra0@gmail.com>
2022-11-29 21:16:14 +0530

    User page added

--- src/Pages/Single/Single.jsx
+++ src/Pages/Single/Single.jsx
@@ -45,7 +45,7 @@ const Single = () => {
             </div>
           </div>
           <div className='right'>
-            <Chart aspect={3 / 1} title='User Spending ( Last 6 Months)' />
+            <Chart aspect={2 / 1} title='User Spending ( Last 6 Months)' />
           </div>
         </div>
         <div className='bottom'>
